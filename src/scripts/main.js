34e17f0942c05cf7de8d07f4f238df3fa16046bf
rafaelranery <rafaelrapnery@gmail.com>
2023-03-20 13:49:55 -0300

    contador

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('olá mundo')
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMS = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (diasEmMS));
+    // console.log(diasAteOEvento);
+
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % (diasEmMS)) / (horasEmMs));
+    // console.log(horasAteOEvento);
+
+    const minutosAteOEvento = Math.floor(((distanciaAteOEvento % (diasEmMS)) % (horasEmMs) / (minEmMs)))
+    // console.log(minutosAteOEvento);
+
+    const segundosAteOEvento = Math.floor((((distanciaAteOEvento % (diasEmMS)) % (horasEmMs) % (minEmMs)) / 1000))
+    // console.log(segundosAteOEvento);
+
+    document.querySelector('#contador').innerHTML = `${diasAteOEvento} dias ${horasAteOEvento}h ${minutosAteOEvento}min ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.querySelector('#contador').innerHTML = `Evento Expirado!`;
+    }
+}, 1000);
